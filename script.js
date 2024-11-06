7bd2c023363d9a05d81270e696dc2dd6e5e4cce4
Saidislombek-Abdumalikov <saidislomabdumalikov1234@gmail.com>
2024-11-06 16:49:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeLanguage(language) {
+    if (language === "uz") {
+        document.getElementById("school-name").textContent = "78-Sonli Maktab Namangan";
+        document.getElementById("about-title").textContent = "Biz haqimizda";
+        // Continue changing content in Uzbek...
+    }
+    else if (language === "ru") {
+        // Add Russian translations
+    }
+    else if (language === "en") {
+        // Add English translations
+    }
+}
